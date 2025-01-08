b9a99abc666dc98ec55e1570f526558bb1b3b128
Abhranil Poddar <111551143+Abhranil2003@users.noreply.github.com>
2025-01-09 00:06:25 +0530

    Create visualization.py

---
+++ visualization.py
@@ -0,0 +1,20 @@
+from tabulate import tabulate
+
+def display_table(data):
+    """
+    Displays the data in a tabular format using the tabulate library.
+
+    Args:
+        data (list): A list of dictionaries containing scan results.
+    """
+    if not data:
+        print("No data to display.")
+        return
+
+    # Prepare headers from keys of the first dictionary
+    headers = data[0].keys()
+    
+    # Prepare rows from the values of each dictionary
+    rows = [list(entry.values()) for entry in data]
+    
+    print(tabulate(rows, headers=headers, tablefmt="grid"))
